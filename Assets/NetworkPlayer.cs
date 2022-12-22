45d8d45b15a1712977f7f668bfcb5b1bdec625fc
mayzyo <25147960+mayzyo@users.noreply.github.com>
2022-12-22 20:29:00 +1000

    :sparkles: geting steam vr to grip

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using Photon.Pun;
 using Unity.XR.CoreUtils;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
 
 public class NetworkPlayer : MonoBehaviour
 {
@@ -31,11 +33,11 @@ public class NetworkPlayer : MonoBehaviour
     public bool isPlayer = false;
     void Start()
     {
-        XROrigin rig = FindObjectOfType<XROrigin>();
+        Player rig = FindObjectOfType<Player>();
         photonView = GetComponent<PhotonView>();
-        headRig = rig.transform.Find("Camera Offset/Main Camera");
-        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
-        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+        headRig = rig.transform.Find("SteamVRObjects/VRCamera");
+        leftHandRig = rig.transform.Find("SteamVRObjects/LeftHand");
+        rightHandRig = rig.transform.Find("SteamVRObjects/RightHand");
 
         if (photonView.IsMine)
         {
@@ -55,11 +57,26 @@ public class NetworkPlayer : MonoBehaviour
             MapPosition(head, headRig);
             MapPosition(leftHand, leftHandRig);
             MapPosition(rightHand, rightHandRig);
-
+            
             UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
             UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+
+            UpdateSteamHandAnimation(SteamVR_Input_Sources.LeftHand, leftHandAnimator);
+            UpdateSteamHandAnimation(SteamVR_Input_Sources.RightHand, rightHandAnimator);
         }
 
+    }
+
+    void UpdateSteamHandAnimation(SteamVR_Input_Sources targetDevice, Animator handAnimator)
+    {
+        if (SteamVR_Actions.default_GrabGrip.GetState(targetDevice))
+        {
+            handAnimator.SetFloat("Grip", 100);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
     }
 
     void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
